3ba1fb5c94f37e4f4ae86fce2f3bc7e57489d1f8
ashwindhevaraj <ashwindhevaraj@gmail.com>
2026-06-11 08:33:13 +0530

    classpojoimprovementsdone
    classpojoimprovementsdone

--- src/test/java/com/qa/test/Pojoimprovements.java
+++ src/test/java/com/qa/test/Pojoimprovements.java
@@ -1,26 +1,23 @@
 package com.qa.test;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.playwright.APIRequest;
 import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.APIResponse;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.RequestOptions;
-import com.qa.main.User;
+import com.qa.main.Users;
 
-public class Pojoclassimplementation {
-	//class 6 
-	User user=new User("ashtestuser",randomemail(),"male","active");
+public class Pojoimprovements {
+	//class 7- involving lombok dependency to reduce code length in users java class
+	//User user=new User("ashtestuser",randomemail(),"male","active");
 	Playwright playwright;
 	APIRequest apirequest;
 	APIRequestContext context;
@@ -32,23 +29,29 @@ public class Pojoclassimplementation {
 	}
 	@Test
 	public void requesttwochecktest() throws IOException {
+		Users users=Users.builder()
+				.name("newusertest1")
+				.email(randomemail())
+				.gender("male")
+				.status("active").build();
+		
 		APIResponse apiresponse = context.post("https://gorest.co.in/public/v2/users",
 				RequestOptions.create().setHeader("Accept","application/json")
 				.setHeader("Authorization", "Bearer e0c6e96fe92eebe84c94e7f8e67fbc7393aed986dea991b02974902c102e39d8")
-.setData(user)); //serialization not needed - set data will handle inbuilt conversion of java object to json
+.setData(users)); //serialization not needed - set data will handle inbuilt conversion of java object to json
 		
 		//Deserialization zone
 		String apiresponsestring = apiresponse.text();
 		ObjectMapper obj=new ObjectMapper();
-		User respuser=obj.readValue(apiresponsestring,User.class); //readvalue converts json to java object
+		Users respuser=obj.readValue(apiresponsestring,Users.class); //readvalue converts json to java object
 		
 		//verification of returned pojo class
 		System.out.println(apiresponsestring); //printing output
 		
-		Assert.assertEquals(user.getEmail(), respuser.getEmail());
-		Assert.assertEquals(user.getName(), respuser.getName());
-		Assert.assertEquals(user.getGender(),respuser.getGender());
-		Assert.assertEquals(user.getStatus(),respuser.getStatus());
+		Assert.assertEquals(users.getEmail(), respuser.getEmail());
+		Assert.assertEquals(users.getName(), respuser.getName());
+		Assert.assertEquals(users.getGender(),respuser.getGender());
+		Assert.assertEquals(users.getStatus(),respuser.getStatus());
 		Assert.assertNotNull(respuser.getId());
 }
 	@AfterTest
